0764de6ceb310fca7341382790785fc9843fdf6d
GitHub Action <action@github.com>
2021-01-15 03:17:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/dancon/hellointelljplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dancon/hellointelljplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dancon.hellointelljplugin.services
 
+import com.github.dancon.hellointelljplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
